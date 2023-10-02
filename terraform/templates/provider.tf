bec6b35a3a7c1d3cfc41155a362e50d8a0c71165
Samuel Olayinka <olayinkasamuel44@gmail.com>
2023-10-02 12:23:20 -0700

    Modified terraform script that spin up AWS fargate

--- terraform/templates/provider.tf
+++
@@ -1,8 +0,0 @@
-# provider.tf
-
-# Specify the provider and access details
-provider "aws" {
-    access_key = var.aws_access_key
-    secret_key = var.aws_secret_key
-    region                  = var.aws_region
-}
--- variables.tf
+++ variables.tf
@@ -35,6 +35,7 @@ variable "app_image" {
 variable "app_port" {
     description = "Port exposed by the docker image to redirect traffic to"
     default = 3000
+
 }
 
 variable "app_count" {
